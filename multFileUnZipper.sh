b3a507c66dd7e4ef91b7e4c0467225976c1e6b7f
Maxim Budzhak <MaximBudzhak3@gmail.com>
2025-08-03 11:03:28 +0100

    Add files via upload

--- multFileUnZipper.sh
+++ multFileUnZipper.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+
+
+
+
+#TODO:
+#Create  a function so that while the file contents exists in the destination, update the
+#name of the new file with number at end
+
 Destination=$(pwd)
 Source=$(pwd)
-RemoveOriginal=false
-Seperate=false
-#Options
+RemoveOriginal=0
+Seperate=0
 
+#Options
 while getopts "d:s:rm" option; do
 	case $option in
 		d) Destination=$OPTARG;;
 		s) Source=$OPTARG;;
-		m) Seperate=true;;
-		r) RemoveOriginal=true;;
+		m) Seperate=1;;
+		r) RemoveOriginal=1;;
 		\?) echo "Error: Invalid Option!"
 		exit;;
 	esac
 done
-
-#Check if directories exist and if not create the destination.
-
 
 if [ ! -d "$Source" ]; then
 	echo "Error: Source directory does not exist!"
@@ -30,40 +35,59 @@ if [ ! -d "$Destination" ]; then
 	read input
 
 	if [ $input = "Y" ] || [ $input = "y" ]; then
-		mkdir $Destination
+		mkdir "$Destination"
 	else
 		exit
 	fi
 	
 fi
 
-
-
 #MAIN CODE
-echo "Starting unzip..."
-echo "Source: $Source"
-echo "Destination: $Destination"
-
-for i in ./*; do
+#TODO: Check if a folder already exists then rename the new folder to something different
+for i in "$Source"/*; do
 	file=$(basename "$i")
-	
-	if [[ $file = *.zip  ]]; then
-		echo "unzipping $file ..."
+	mime=$( file -b --mime-type "$i")
+	mime="${mime##*/}"
+	remove=1
+	case "$mime" in
 
-		if [$Seperate]; then
-			unzip $file -d $Destination"/$file"
-		else
-			unzip $file -d $Destinationi
+		"zip")
+			#unzip zip
+			
+			if [ $Seperate = 1 ]; then
+				newFileName=${file%".zip"}
+				unzip "$Source/$file" -d "$Destination/$newFileName"
+			else
+				#if [-d "$Destination/$file" ]
+				unzip "$Source/$file" -d "$Destination"
+			fi;;
+		"x-tar")
+			#unzip tar
+			#if [-d "$Destination/$file" ]
+			tar -xf "$Source/$file" -C "$Destination";;
+		"x-7z-compressed")
+			#unzip tar
+			#if [-d "$Destination/$file" ]
+			7z x "$Source/$file" -o"$Destination";;
+
+		"x-bzip2")
+			#unzip tar
+			#if [-d "$Destination/$file" ]
+			tar -xjf "$Source/$file" -C "$Destination";;
+
+		"gzip")
+			#unzip tar.gz
+			#if [-d "$Destination/$file" ]
+			tar -xzf "$Source/$file" -C "$Destination";;
+
+		*)
+			remove=0;;
+
+
+	esac
+
+		if (( $RemoveOriginal == 1  &&  $remove == 1 )); then
+			rm "$i"
 		fi
-		echo "Successfully unzipped $file"
-		
-
-		if [$RemoveOriginal]; then
-			echo "Removing"
-			rm $i
-			echo "Removed"
-		fi
-
-	fi
 done
 
